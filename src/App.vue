eaeeca42ccd3ffc34c646b4b7c9130cb5ddf60f6
Leonardo <leonardodipace@gmail.com>
2024-03-18 14:12:32 -0400

    Imported Chart component

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Chart from "./components/Chart.vue";
+</script>
+
+<template>
+  <div class="App">
+    <Chart />
+  </div>
+</template>
+
+<style scoped>
+.App {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
